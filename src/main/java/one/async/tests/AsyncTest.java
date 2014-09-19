7bc064acd63baa9f63e343e88d3769aebdc3ec73
mxro <mxro@nowhere.com>
2014-09-19 14:52:47 +1000

    Adding capability to instantiate AtomicIntegers Autocommit Fri Sep 19 14:52:47 EST 2014

--- src/main/java/one/async/tests/AsyncTest.java
+++ src/main/java/one/async/tests/AsyncTest.java
@@ -1,7 +1,7 @@
 package one.async.tests;
 
 import de.mxro.concurrency.Concurrency;
-import de.mxro.concurrency.TimerFactory;
+import de.mxro.concurrency.factories.TimerFactory;
 
 /**
  * Base interface for helper classes to support asynchronous unit tests.
